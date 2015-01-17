71ba1806cd2d1e27a7fe3e31d2d735acc8d979d9
Fabio Pelosin <fabiopelosin@gmail.com>
2015-01-17 09:40:23 +0100

    [Fabio/Shell] Add Android Studio configuration

---
+++ zsh/fabio_android.zsh
@@ -0,0 +1,4 @@
+ANDROID_HOME="/Users/fabio/Library/Android/sdk"
+ANDROID_TOOLS="/Users/fabio/Library/Android/sdk/tools"
+ANDROID_PLATFORM_TOOLS="/Users/fabio/Library/Android/sdk/platform-tools"
+PATH=$PATH:$ANDROID_TOOLS:$ANDROID_PLATFORM_TOOLS
